0e606133215066a62a405d86cd3586398fa5186e
WiktorGap <148978926+WiktorGap@users.noreply.github.com>
2025-01-23 20:33:28 +0100

    Update and rename testowa_tabela.sql to table_mod.sql

---
+++ table_mod.sql
@@ -0,0 +1,20 @@
+ALTER TABLE users ADD COLUMN age int;
+ALTER TABLE users ADD COLUMN user_id SERIAL PRIMARY KEY;
+ALTER TABLE users DROP COLc
+
+CREATE TABLE users(user_id SERIAL PRIMARY KEY,age int , name_ VARCHAR(255), sur_name VARCHAR(255), email VARCHAR(255) UNIQUE NOT NULL , phone_no int UNIQUE NOT NULL );  
+
+
+INSERT INTO users(age, name_, sur_name, email, phone_no) 
+VALUES (15, 'Maciek', 'Cwalina', 'maciekcwalina@gmail.com', 1389103);
+
+
+
+CREATE VIEW testowy_widok AS
+SELECT user_id, age, name_
+FROM users;
+
+
+
+select * from testowy_widok;
+
