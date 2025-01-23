05c67b0b5149a7e56058b57a7a3f12a5a8c10bf1
WiktorGap <148978926+WiktorGap@users.noreply.github.com>
2025-01-23 20:29:20 +0100

    Delete SQLQuery16.sql

--- SQLQuery16.sql
+++
@@ -1,7 +0,0 @@
---Oblicz œredni¹ cenê produktów w ka¿dej kategorii produktów. [Production.Product] i [Production.ProductCategory]
-
-select 
-AVG(p.ListPrice) as avgprice,
-p.ProductSubcategoryID
-from Production.Product as p 
-Group by p.ProductSubcategoryID
