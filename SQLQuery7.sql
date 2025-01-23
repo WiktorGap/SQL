ccb9b8fa46a81d7381f063682cc93d05c1f065a2
WiktorGap <148978926+WiktorGap@users.noreply.github.com>
2025-01-23 20:30:33 +0100

    Delete SQLQuery7.sql

--- SQLQuery7.sql
+++
@@ -1,4 +0,0 @@
---Utwórz zapytanie  indeks na kolumnie HireDate w tabeli HumanResources.Employee 
---w celu optymalizacji zapytañ filtruj¹cych po tej kolumnie.
-
-CREATE INDEX IX_HireDate ON HumanResources.Employee (HireDate)
